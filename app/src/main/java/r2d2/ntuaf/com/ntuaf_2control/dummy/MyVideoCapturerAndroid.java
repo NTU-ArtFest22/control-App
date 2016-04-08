27352c1af36ca8e9653e84ef101ea6aee13393cb
turing <kevin831130810620@gmail.com>
2016-04-09 03:54:14 +0800

    add uvc library

---
+++ app/src/main/java/r2d2/ntuaf/com/ntuaf_2control/dummy/MyVideoCapturerAndroid.java
@@ -0,0 +1,12 @@
+package r2d2.ntuaf.com.ntuaf_2control.dummy;
+
+import org.webrtc.VideoCapturerAndroid;
+
+/**
+ * Created by turing on 2016/4/5.
+ */
+public class MyVideoCapturerAndroid extends VideoCapturerAndroid {
+    private MyVideoCapturerAndroid() {
+        super();
+    }
+}
